8183ae4c609ab08cbdfd4b288c50ab839ad330de
Vinoth Kannan <vinothkannan@vinkas.com>
2016-10-01 15:52:34 +0530

    constructor overloading not supported in php

--- src/API.php
+++ src/API.php
@@ -10,11 +10,6 @@ class API
 
   use TraitAPI;
 
-  function __construct(Forum $forum) {
-    $this->setUrl($forum->domain, $forum->ssl);
-    $this->setQueryParams($forum->api_key, $forum->api_username);
-  }
-
   function __construct($domain, $ssl, $api_key, $api_username) {
     $this->setUrl($domain, $ssl);
     $this->setQueryParams($api_key, $api_username);
